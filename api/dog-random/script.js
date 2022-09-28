e214ef7b1461dcf6deda866fe5a078b79159d335
lei <mia.wang90@gmail.com>
2022-09-28 18:51:30 +0900

    Finish promise async await

--- api/dog-random/script.js
+++ api/dog-random/script.js
@@ -9,12 +9,19 @@ const dogImageDiv = document.getElementById('dogImage')
 const dogButton = document.getElementById('dogButton')
 
 // stuff you have to wait for
-const getNewDog = () => {
-  fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(data => {
-      dogImageDiv.innerHTML = `<img src='${data.message}' height=300 width=300/>`
-    })
+// const getNewDog = () => {
+//   fetch('https://dog.ceo/api/breeds/image/random')
+//     .then(response => response.json())
+//     .then(data => {
+//       dogImageDiv.innerHTML = `<img src='${data.message}' height=300 width=300/>`
+//     })
+// }
+
+const getNewDog = async () => {
+  const url = 'https://dog.ceo/api/breeds/image/random'
+  const response = await fetch(url)
+  const data = await response.json()
+  console.log(data)
 }
 
 dogButton.onclick = () => getNewDog()
